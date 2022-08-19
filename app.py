1289cc7f339ab0f48d60408986683bb787013536
Hae-Dal <jyh4282002@naver.com>
2022-08-19 21:24:07 +0900

    모듈화

--- app.py
+++ app.py
@@ -6,19 +6,17 @@ from models import Stores
 
 app = Flask(__name__)
 
-@app.route('/dbtoweb')
+@app.route('/dbtoweb', methods=['POST','GET'])
 # route() : 외부 웹브라우져에서 웹서버로 접근 시 해당 주소로 입력을 하게 되면 특정 함수가 실행되게 도와줌('/test': /test 주소에 접근하면 아래 함수 실행)
 def dbToWeb(): # DB에서 웹으로 데이터를 보내기위한 함수
-    data = Stores.get_all()
-    return json.dumps(data, default=json_default)
+    data = Stores.get_all() # models.py의 Stores클래스 내의 get_all함수 실행
+    return json.dumps(data, default=json_default) # json 형식으로 인코딩 후 String으로 변환해서 리턴
 
 def json_default(value): # DB 내 데이터 형식 중 JSON으로 변환 불가능한 형식이 있을때, STRING으로 바꿔줌
     if isinstance(value, datetime.date):
-        return value.strftime('%Y-%m-%d') # strftime : 날짜, 시간을 string으로 변환ㅊ
+        return value.strftime('%Y-%m-%d') # strftime : 날짜, 시간을 string으로 변환
     raise TypeError('not JSON serializable')
 
-#@app.route('/andtodb',methods=['POST','GET']) 
-    
 
 
 
--- models.py
+++ models.py
@@ -3,16 +3,16 @@
 import pymysql
 
 class Stores():
-    def get_all():
+    def get_all(): # DB에 있는 데이터 전부 꺼내오는 함수
         conn = pymysql.connect(host='localhost', user='root', password='05020130lee@', db='Boards', charset='utf8') # mysql 연결
         if conn.open: # DB 연결 여부 확인
             print('connected')
         
         curdic = conn.cursor(pymysql.cursors.DictCursor) # sql문을 입력할 cursor 생성
-        sql = "SELECT * FROM posttbl"
-        curdic.execute(sql)
+        sql = "SELECT * FROM posttbl" # sql문
+        curdic.execute(sql) # sql문 실행
         
-        store_lists = curdic.fetchall()
+        store_lists = curdic.fetchall() # 전부 fetch
         conn.close() # DB 연결 해제
-        return store_lists
+        return store_lists # fetch한 내용을 리턴
         
