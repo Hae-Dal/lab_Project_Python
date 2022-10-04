a3400b9abc5cc5c5b6114fa91b1ee92f77df55b9
HAEDAL <jyh4282002@naver.com>
2022-10-04 17:07:02 +0900

    like 버튼 클릭 구현

--- app.py
+++ app.py
@@ -5,27 +5,31 @@ from flask  import Flask, json, request
 import datetime
 from models import Stores, sendData
 
+
 app = Flask(__name__)
 
-@app.route("/", methods = ['POST'])
-def helloWorld():
-    return "helloWorld"
+@app.route('/dbtoweb', methods=['POST','GET'])
+def dbToWeb():                                                                                  # DB에서 웹으로 데이터를 보내기위한 함수 
+    data = Stores.get_all()                                                                     # models.py의 Stores클래스 내의 get_all함수 실행
+    return json.dumps(data, default=json_default, ensure_ascii=False)                           # json 형식으로 인코딩 후 String으로 변환해서 리턴
 
-@app.route('/dbtoweb', methods=['POST'])
-# route() : 외부 웹브라우져에서 웹서버로 접근 시 해당 주소로 입력을 하게 되면 특정 함수가 실행되게 도와줌('/test': /test 주소에 접근하면 아래 함수 실행)
-def dbToWeb(): # DB에서 웹으로 데이터를 보내기위한 함수
-    data = Stores.get_all() # models.py의 Stores클래스 내의 get_all함수 실행
-    return json.dumps(data, default=json_default) # json 형식으로 인코딩 후 String으로 변환해서 리턴
-
-def json_default(value): # DB 내 데이터 형식 중 JSON으로 변환 불가능한 형식이 있을때, STRING으로 바꿔줌
+def json_default(value):                                                                        # DB 내 데이터 형식 중 JSON으로 변환 불가능한 형식이 있을때, STRING으로 바꿔줌
     if isinstance(value, datetime.date):
-        return value.strftime('%Y-%m-%d') # strftime : 날짜, 시간을 string으로 변환
+        return value.strftime('%Y-%m-%d')                                                       # strftime : 날짜, 시간을 string으로 변환
     raise TypeError('not JSON serializable')
 
 @app.route('/sendserver', methods=['POST','GET'])
-def toDB(): # DB에 저장
-    id = request.json['userID'] # json형태의 키값중 'userID'의 값을 리턴
-    password = request.json['userPassword'] # json형태의 키값중 'userPassword'의 값을 리턴
-    name = request.json['userName'] # json형태의 키값중 'userName'의 값을 리턴
-    sendData.sendtodb(id,password,name) # models.py의 sendData클래스 내의 sendtodb함수 실행
+def toDB():                                                                                     # DB에 저장
+    title = request.json['title']                                                               # json형태의 키값중 'title'의 값을 리턴
+    contents = request.json['contents']                                                         # json형태의 키값중 'contents'의 값을 리턴
+    userName = request.json['userName']                                                         # json형태의 키값중 'userName'의 값을 리턴
+    time = request.json['time']                                                                 # json형태의 키값중 'time'의 값을 리턴
+    sendData.sendtodb(userName,title,contents,time)                                             # models.py의 sendData클래스 내의 sendtodb함수 실행
     return "Success"
+
+@app.route("/likeup", methods=['POST','GET'])
+def upLike():
+    postnum = request.form['postnum']
+    sendData.upLike(int(postnum))
+    return "Success"
+    
--- models.py
+++ models.py
@@ -4,25 +4,55 @@ import pymysql
 from dbConnect import dbcon, dbclose
 
 class Stores():
-    def get_all(): # DB에 있는 데이터 전부 꺼내오는 함수
-        conn = dbcon() # db연결 함수
-        curdic = conn.cursor(pymysql.cursors.DictCursor) 
-        # sql문을 입력할 cursor 생성
-        sql = "SELECT * FROM posttbl" # sql문
-        curdic.execute(sql) # sql문 실행
-        
-        store_lists = curdic.fetchall() # 전부 fetch
-        dbclose(conn) # DB 연결 해제
-        return store_lists # fetch한 내용을 리턴
+    def get_all():                                                  # DB에 있는 데이터 전부 꺼내오는 함수
+        conn = dbcon()                                              # db연결 함수
+        curdic = conn.cursor(pymysql.cursors.DictCursor)            # sql문을 입력할 cursor 생성
+        sql = "SELECT * FROM Post"                                  # sql문
+        curdic.execute(sql)                                         # sql문 실행
+        store_lists = curdic.fetchall()                             # 전부 fetch
+        dbclose(conn)                                               # DB 연결 해제
+        return store_lists                                          # fetch한 내용을 리턴
+    
+    def post_count():                                               # DB의 가장 마지막 postnum을 가져오는 함수
+        conn = dbcon()
+        curdic = conn.cursor(pymysql.cursors.DictCursor)
+        sql = "SELECT postnum FROM Post order by postnum DESC limit 10000"
+        curdic.execute(sql)
+        postnum = curdic.fetchone()
+        print(postnum)
+        dbclose(conn)
+        return postnum
+
+    def get_likenum(postnum):
+        conn = dbcon()
+        curdic = conn.cursor(pymysql.cursors.DictCursor)
+        sql = "SELECT likenum FROM Post WHERE postnum=%s"
+        curdic.execute(sql, (postnum))
+        likenum = curdic.fetchone()
+        print(postnum, ", ", likenum)
+        dbclose(conn)
+        return likenum['likenum']
 
 class sendData():
-    def sendtodb(id,postnum,posttitle,postcontents,postdate,postvisit,postlike):
-        conn = dbcon() # db연결 함수
-        data = (id,postnum,posttitle,postcontents,postdate,postvisit,postlike) 
-        # 데이터 튜플형식으로 변경
-        curdic = conn.cursor() # sql문을 입력할 cursor 생성
-        sql = "INSERT INTO posttbl VALUES(%s,%d,%s,%s,%s,%d,%d)" # sql문
-        curdic.execute(sql, data) # sql문 실행
-        conn.commit() # 수정사항 반영
-        dbclose(conn) # DB 연결 해제
+    def sendtodb(name,title,contents,time):
+        count = int(Stores.post_count()) + 1
+        likenum = 0
+        conn = dbcon()                                              # db연결 함수
+        data = (count,name,title,contents,time,likenum)             # 데이터 튜플형식으로 변경
+        curdic = conn.cursor()                                      # sql문을 입력할 cursor 생성
+        sql = "INSERT INTO Post VALUES(%s,%s,%s,%s,%s,%s)"          # sql문
+        curdic.execute(sql, data)                                   # sql문 실행
+        conn.commit()                                               # 수정사항 반영
+        dbclose(conn)                                               # DB 연결 해제
         return print("sendtodb OK")
+
+    def upLike(postnum):                                            # 좋아요 버튼 클릭시 likenum 수 증가 함수
+        likenum = Stores.get_likenum(postnum) + 1             
+        conn = dbcon()
+        curdic = conn.cursor()
+        print(likenum)
+        sql = "UPDATE Post SET likenum=%s WHERE postnum=%s"
+        curdic.execute(sql, (likenum,postnum))
+        conn.commit()
+        dbclose(conn)
+        return print("upLike : success")
